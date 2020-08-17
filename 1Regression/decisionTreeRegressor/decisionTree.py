4d2daa92ef3f1dd47bc28ca3f97f64ac444d277a
Encry <encry973r@gmail.com>
2020-08-17 01:07:16 +0100

    push with pickle

--- 1Regression/decisionTreeRegressor/decisionTree.py
+++ 1Regression/decisionTreeRegressor/decisionTree.py
@@ -7,10 +7,18 @@ Created on Thu Jun  4 16:29:07 2020
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pickle
+import os.path
 
 data = pd.read_csv('Gaming_data.csv')
 X = data.iloc[:, 0:1].values
 Y = data.iloc[:, 1:2].values
+
+# if model already exists, load it for use
+if os.path.exists("trained_model.pickle"):
+    print("Loading Trained Model")
+    model = pickle.load(open("trained_model.pickle", "rb"))
+    
 
 """
 no need for feature scaling because DT does does not utilize euclidean distances
@@ -20,6 +28,10 @@ no need!!!!
 from sklearn.tree import DecisionTreeRegressor
 regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, Y)
+
+# save model to file for future use
+with open("trained_model.pickle", "wb") as file:
+    pickle.dump(regressor, file)
 
 Y_pred = regressor.predict([[7.5]])
 
